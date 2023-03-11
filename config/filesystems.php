3239545606750bb7bb3e5bcaf82a770c5d6be5fc
alessflame <francescoalessi.code@libero.it>
2023-03-11 14:46:51 +0100

    change

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,7 +39,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => 'https://larajobs-alessflame.vercel.app'.'/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
